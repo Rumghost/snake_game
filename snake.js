c6482d64a5d848464014305595dd92819d9e8293
Rumghost <leighton23rock@gmail.com>
2022-06-04 10:47:43 -0500

    step 4
    food disappear when eaten

--- snake.js
+++ snake.js
@@ -1,11 +1,8 @@
 import { getInputDirection } from "./input.js"
 
 export const SNAKE_SPEED = 2
-const snakeBody = [
-    {x:10, y:11},
-    {x:11, y:11},
-    {x:12, y:11}
-]
+const snakeBody = [{x:11, y:11}]
+let newSegments = 0
 
 export function update() {
     const inputDirection = getInputDirection()
@@ -25,4 +22,18 @@ export function draw (gameBoard) {
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)
     })
+}
+
+export function expandSnake(amount) {
+    newSegments += amount
+}
+
+export function onSnake(position) {
+    return snakeBody.some(segment => {
+        return equalPositions(segment, position)
+    })
+}
+
+function equalPositions( pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y
 }
